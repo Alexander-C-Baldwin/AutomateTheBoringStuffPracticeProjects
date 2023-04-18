b309ba1341ce07693d83eba0bb93c8d33e34dd57
Alexander Baldwin <61666275+Alexander-C-Baldwin@users.noreply.github.com>
2023-04-17 23:53:54 -0500

    Add files via upload

---
+++ characterPictureGridPracticeProjectQuestion.py
@@ -0,0 +1,20 @@
+# will use a list of lists to print an image from that grid using indices
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for x in range(6):
+
+    for y in range(9):
+        if y != 8:
+            print(grid[y][x], end = '')
+        else:
+            print(grid[8][x])
+
